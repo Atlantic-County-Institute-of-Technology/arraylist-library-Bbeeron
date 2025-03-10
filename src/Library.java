2010e29e00fc1b2d8287d7df786072caa35167bb
Byron Manuel <99151273+Bbeeron@users.noreply.github.com>
2025-03-09 23:40:30 -0400

    Finished (w/Comments)

--- src/Library.java
+++ src/Library.java
@@ -1,23 +1,23 @@
-import java.util.*;
+import java.util.*;     // Imports all libraries
 
-public class Library {
+public class Library {      // Library class is referenced for its arrayList commands
 
-    private final ArrayList<String> playList = new ArrayList<>();
-    Scanner myObj = new Scanner(System.in);
+    private final ArrayList<String> playList = new ArrayList<>();       // Global variable making an empty arraylist
+    Scanner myObj = new Scanner(System.in);     // For telling what the user wants to do
 
     public Library(){
         System.out.println("Let's initialize this playlist! \n" +
                     "When you're done populating, type 'n': ");
         String done = "";
 
-        while (!(done.equals("n"))) {
+        while (!(done.equals("n"))) {       // While loop forces user to keep inputting songs into the arraylist
 
                 String songName = myObj.nextLine();
 
                 if (songName.equals("n")) {
                     done = "n";
                 } else {
-                    playList.add(songName);
+                    playList.add(songName);     // Allows the user to initialize an arraylist with items already in it
                 }
 
             }
@@ -25,40 +25,40 @@ public class Library {
 
     public ArrayList<String> getPlayList() {
         return playList;
-    }
+    }   // Returns the playlist allowing user to view the contents
 
     public void addItem(String item){
         playList.add(item);
-    }
+    }   //  Appends an item at the end of the playlist
 
     public void removeItem(String item){
         playList.remove(item);
-    }
+    }   // Removes an item based on user input
 
     public String getNextItem(){
         return playList.getFirst();
-    }
+    }   // Return the first item in the arraylist
 
     public void skipNextItem(){
         playList.removeFirst();
-    }
+    }  // Removes the first item on the playlist
 
     public void sortAlphabet(){
         playList.sort(String::compareTo);
-    }
+    }  // Sorts the items alphabetically (uppercase > lowercase)
 
     public int find(String item){
         return playList.indexOf(item);
-    }
+    }   // Returns index of item inputted
 
     public void shuffle(){
         Collections.shuffle(playList);
-    }
+    }       // Shuffles the playlist using command from library Collections
 
 
 
     @Override
     public String toString() {
-        return "Playlist: " + playList;
-    }
+        return "\nPlaylist: " + playList;
+    }       // Formats return statement
 }
--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,79 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
-
-        Library playList = new Library();
+        String songName;        // Initializes a string
+        Library playList = new Library();  // Creates an empty arraylist populated by the user
 
         System.out.println(playList);
 
 
         int stop = 0;
-        while (stop == 0) {
+        while (stop == 0) {         // While loop continues until user exits
+            songName = playList.getNextItem();
+            // List of commands for the user
+            System.out.println("""
+                    
+                     Indicate an action (#):\s       
+                    1. Play\s
+                    2. Skip\s
+                    3. Add\s
+                    4. Remove\s
+                    5. Sort\s
+                    6. Shuffle\s
+                    7. Find\s
+                    8. Exit\s
+                    """);
 
-            System.out.println("\n Indicate an action (#): \n" +
-                            "1. Play \n" +
-                            "2. Skip \n" +
-                            "3. Add \n" +
-                            "4. Remove \n" +
-                            "5. Sort \n" +
-                            "6. Shuffle \n" +
-                            "7. Find \n" +
-                            "8. Exit \n");
+            String option = myObj.nextLine();
+            switch (option) {       // Switch cases provides different scenarios for the different turns
+                case "1" -> System.out.println("Now playing: " + songName);     // "Pops" the first song and print it as a string
+                case "2" -> {
+                    System.out.println("Skipped: " + songName);     // Removes the current song and plays the next by removing the first
+                    playList.skipNextItem();
+                    songName = playList.getNextItem();
+                    System.out.println("Now playing: " + songName);
+                }
+                case "3" -> {       // Add a new song
+                    System.out.println("What to add in playlist: ");
+                    String item = myObj.nextLine();
+                    playList.addItem(item);
+                    System.out.print(playList.getPlayList());
+                }
+                case "4" -> {       // Removes a song
+                    System.out.println("What to remove in playlist: ");
+                    String item = myObj.nextLine();
+                    playList.removeItem(item);
+                    System.out.print(playList.getPlayList());
+                }
+                case "5" -> {           // Sorts songs alphabetically
+                    System.out.println(playList.getPlayList());
+                    playList.sortAlphabet();
+                    System.out.println(playList.getPlayList());
+                }
+                case "6" -> {           // Randomizes order
+                    System.out.println(playList.getPlayList());
+                    playList.shuffle();
+                    System.out.println(playList.getPlayList());
+                }
+                case "7" -> {           // Finds whether a song is in the playlist
+                    System.out.println("Find: ");
+                    String item = myObj.nextLine();
+                    int position = playList.find(item);
+                    if (position == -1) {
+                        System.out.println("Item not found!");
+                    } else {
+                        System.out.println(position);
+                    }
 
-            
+                }
+                case "8" -> {       // Exits loop and ends code
+                    System.out.println("Bye, bye!");
+                    stop = 1;
+                }
+                default -> System.out.println("Wrong input!");      // Error Checking
+            }
 
-
-
+//         Embellishments that may be added (or not)
 //            System.out.println("\nThis is Stoopify, let's create your playlist! \n" +
 //                    "1. Create a new playlist \n" +
 //                    "2. View playlists");
